d451143945d50d797c0d26a666b5ce0f8017b2c1
ninambash <ninambash@gmail.com>
2022-12-01 17:05:31 -0800

    Updated jsfile

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+console.log("Hello World");
+const squares = document.querySelectorAll(".grid div");
+const result = document.querySelector("result");
+const displayCurrentPlayer = document.querySelector("current-player");
+let currentPlayer = 1;
+
+////add onclick to all our squares using a for loop
+
