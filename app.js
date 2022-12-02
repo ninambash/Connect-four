65fe56ed105f7dbee4968cb2bd52ac5937dc3de2
ninambash <ninambash@gmail.com>
2022-12-01 18:44:24 -0800

    updated

--- app.js
+++ app.js
@@ -5,4 +5,25 @@ const displayCurrentPlayer = document.querySelector("current-player");
 let currentPlayer = 1;
 
 ////add onclick to all our squares using a for loop
-
+for (let i = 0; i < squares.length; i++) {
+    squares[i].onclick = () => {
+      ///if the square below your current square is taken go ontop of it
+      if (squares[i + 7].classList.contains("taken")) {
+        if (currentPlayer == 1) {
+          squares[i].classList.add("taken");
+          squares[i].classList.add("player-one");
+          currentPlayer = 2;
+          displayCurrentPlayer.innerHTML = currentPlayer;
+  
+          //logic for player 2
+        } else if (currentPlayer == 2) {
+          squares[i].classList.add("taken");
+          squares[i].classList.add("player-two");
+          currentPlayer = 1;
+          displayCurrentPlayer.innerHTML = currentPlayer;
+        } else alert("cant");
+      }
+      //check if it works
+      ///console.log("you have clicked sqaure" + i)
+    };
+  }
