dbd0eec51647929249fdec4c20530f8b678456ab
ninambash <ninambash@gmail.com>
2022-12-04 17:39:16 -0800

    added a newGame function

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 ////check if everything is connected
 console.log("Hello Connect Four")
+///create a function for starting the game 
+////create a class for slots
+///add and append class
+///have 2 elements red and yellow
+//create an empty array that stores columns and rows
+///determine the next color
+///make elements clickable and not clickable if already filled
+///have a check win function 
+///have d draw function
+///game over function
+///check if game is over
+
+
+//create a function for starting the game 
+function newGame(button) {
+  button.style.display = "none";
+  const game = document.getElementById("gameContainer")
+}
+newGame()
